ac73de0fdd6f6da3522855f0cfcae9b485a873e0
ShinAquila <augusto08rivera12@gmail.com>
2024-04-18 14:21:46 +0800

    added retrieve

--- app/src/main/java/com/example/simplecredentials/retrieve.java
+++ app/src/main/java/com/example/simplecredentials/retrieve.java
@@ -2,7 +2,6 @@ package com.example.simplecredentials;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,67 +14,70 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.internal.EdgeToEdgeUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class retrieve extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_retrieve);
 
+        EditText txt_id = findViewById(R.id.txt_id);
         EditText txt_uname = findViewById(R.id.txt_uname);
         EditText txt_pword = findViewById(R.id.txt_pword);
         EditText txt_email = findViewById(R.id.txt_email);
-        Button btn_save = findViewById(R.id.btn_save);
+        Button btn_retrieve = findViewById(R.id.btn_retrieve);
 
-        Button btn_toUpdate = findViewById(R.id.btn_toUpdate);
-        Button btn_toRetrieve = findViewById(R.id.btn_toRetrieve);
-        btn_toUpdate.setOnClickListener(new View.OnClickListener() {
+        btn_retrieve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openUpdate();
-            }
-        });
-        btn_toRetrieve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openRetrieve();
-            }
-        });
-
-        btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                String id = txt_id.getText().toString();
                 String uname = txt_uname.getText().toString();
                 String pword = txt_pword.getText().toString();
                 String email = txt_email.getText().toString();
 
                 RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="http://192.168.1.53/android_crud/create.php";
+                String url ="http://192.168.1.53/android_crud/retrieve.php";
                 //local ip/folder name from htdocs/php create file
 
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                if (response.equals("Success")){
-                                    Toast.makeText(MainActivity.this, "Data Added", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(MainActivity.this, "Data Failed to Add to Database", Toast.LENGTH_SHORT).show();
+                                try {
+                                    JSONArray jsonArray = new JSONArray(response);
+
+                                    for (int i = 0; i < jsonArray.length(); i++){
+                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                        String user_name = jsonObject.getString("user_uname");
+                                        String user_pass = jsonObject.getString("user_pword");
+                                        String user_email = jsonObject.getString("user_email");
+
+                                        txt_uname.setText(user_name);
+                                        txt_pword.setText(user_pass);
+                                        txt_email.setText(user_email);
+                                    }
+                                } catch(JSONException e){
+                                    e.printStackTrace();
+                                    Toast.makeText(retrieve.this, response, Toast.LENGTH_SHORT).show();
                                 }
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(retrieve.this, error.toString(), Toast.LENGTH_SHORT).show();
                     }
                 }){
                     protected Map<String, String> getParams(){
                         Map<String, String> paramV = new HashMap<>();
+                        paramV.put("id", id);
                         paramV.put("uname", uname);
                         paramV.put("pword", pword);
                         paramV.put("email", email);
@@ -85,14 +87,5 @@ public class MainActivity extends AppCompatActivity {
                 queue.add(stringRequest);
             }
         });
-    }
-
-    public void openUpdate(){
-        Intent intent = new Intent(this, update.class);
-        startActivity(intent);
-    }
-    public void openRetrieve(){
-        Intent intent = new Intent(this, retrieve.class);
-        startActivity(intent);
     }
 }
